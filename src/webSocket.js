c66f7329692611e26a9669a4aeda1bf46a8c2c67
Henrique <henrique@itfast.com.br>
2024-01-24 14:38:41 -0300

    updated commit

--- src/webSocket.js
+++ src/webSocket.js
@@ -1,31 +1,51 @@
 const express = require("express");
 const cors = require("cors");
 const Stream = require("node-rtsp-stream");
-const http = require("http");
+
+let stream;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const server = http.createServer(app);
+app.get("/start-stream/:channel", (req, res) => {
+	const { channel } = req.params;
 
-server.listen(3010, ()=>{
+	if (stream) {
+		stream.stop();
+	}
 
-  console.log("Server WEBSOCKET running");
+	stream = new Stream({
+		streamUrl: `rtsp://admin:Tendus2021@192.168.15.21:554/cam/realmonitor?channel=${channel}&subtype=0&unicast=true`,
+		wsPort: 9999,
+		ffmpegOptions: {
+			"-stats": "",
+			"-r": 30,
+			"-vf": "scale=640:480",
+			"-bf": 0,
+			"-c:v": "h264",
+			"-rtsp_transport": "tcp",
+			"-f": "image2pipe",
+			"-vcodec": "mjpeg",
+			"-q:v": 2,
+		},
+	});
 
-  stream = new Stream({
-    streamUrl: 'rtsp://admin:Tendus2021@192.168.15.10:554/cam/realmonitor?channel=1&subtype=0&unicast=true',
-    wsPort: 9999,
-    ffmpegOptions:{
-      '-stats':'',
-      '-r': 30,
-      '-vf': 'scale=640:480',
-      '-bf': 0, 
-      '-c:v': 'h264',
-      '-rtsp_transport': 'tcp',
-      '-f': 'image2pipe',
-      '-vcodec': 'mjpeg',
-      '-q:v': 2,
-    }
-  });
+	res.json({
+		error: false,
+		massage: `Start stream in Channel: ${channel}`,
+	});
+});
+
+app.get("/end-stream", (req, res) => {
+	if (stream) {
+		stream.stop();
+		res.send("Connection closed");
+	} else {
+		res.send("Nenhuma conexão para encerrar");
+	}
+});
+
+app.listen(3010, () => {
+	console.log("Server WEBSOCKET running");
 });
