e3d2d58bb1ed491722397ebdebfe356d42358729
Henrique <henrique@itfast.com.br>
2024-01-26 14:08:19 -0300

    updated commit

--- DB/camera-nvr.sql
+++ DB/camera-nvr.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 24/01/2024 às 13:06
--- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.2.4
+-- Tempo de geração: 26/01/2024 às 15:59
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `camera_nvp`
+-- Banco de dados: `camera-nvr`
 --
 
 -- --------------------------------------------------------
@@ -32,7 +32,7 @@ CREATE TABLE `users` (
   `id_` varchar(50) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
+  `password` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -42,7 +42,9 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `id_`, `nome`, `email`, `password`) VALUES
 (1, '12', 'guilhermer', 'guilherme@g.com', '123'),
 (7, 'daa0b4f3-8529-4176-b173-947d4692f556', '', 'teste@g.com', '$2a$10$TC79pOYnOK.voZE/V57Jw.pSoL77WdgdLKezDKxyvBk'),
-(8, 'f1f5af1c-0af9-4919-bf38-8440f1c5bdc9', '', 'Wol@gmail.com', '$2a$10$EZcTS5DWhdNBWSsr5LEbcOHdl5gHZZ9zuHDbYomuQqv');
+(8, 'f1f5af1c-0af9-4919-bf38-8440f1c5bdc9', '', 'Wol@gmail.com', '$2a$10$EZcTS5DWhdNBWSsr5LEbcOHdl5gHZZ9zuHDbYomuQqv'),
+(9, '7afa1cf8-9f76-497d-b9b2-1c657c980f88', '', 'qqq@asd.com', '$2a$10$uCnGkOKFV3sftph6WD3gR.a3S5JMAZl7uZOHVQKrcHy'),
+(11, 'bf92efb2-9193-46e5-b7ab-6f5ca4a93814', '', 'teste@teste.com', '$2a$10$nZ7zlXcc7qyqWTRK2PiNK.E0zaF/yPneQ4YmryirzxLYqPWFG7H6C');
 
 --
 -- Índices para tabelas despejadas
@@ -62,7 +64,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- src/controller/AuthLogin.js
+++ src/controller/AuthLogin.js
@@ -1,17 +1,73 @@
-//Autenticar e permitir logar 
 const express = require("express");
-
+const mysql = require("mysql");
+const validator = require("validator");
+const bcryptjs = require("bcryptjs");
+const db = require("../database");
 const router = express.Router();
 
-router.post("/auth", async (req, res)=>{
+router.post("/auth", async (req, res) => {
+	const { email, password } = req.body;
 
-    const {email, password} = req.body;
+	if (!email || !password) {
+		return res.json({
+			error: true,
+			massage: "Please fill in all fields",
+		});
+	}
 
-    console.log(email, password);
-    return res.json({
-        error:false,
-        massage: "Registred with success!"
-});
+	if (!validator.isEmail(email)) {
+		return res.json({
+			error: true,
+			message: "Email format not accepted",
+		});
+	}
+	try {
+		const AuthUser = await new Promise((resolve, reject) => {
+			db.query(
+				"SELECT * FROM users WHERE email = ?",
+				[email],
+				(err, results) => {
+					if (err) {
+						return reject(err);
+					} else {
+						return resolve(results);
+					}
+				}
+			);
+		});
+
+		if (AuthUser.length > 0) {
+			const DBpassword = AuthUser[0].password;
+			console.log(DBpassword);
+			console.log(password);
+			const passwordValid = await bcryptjs.compare(password, DBpassword);
+			if (passwordValid) {
+				return res.json({
+					error: false,
+					login: true,
+					message: "Approved",
+				});
+			} else {
+				return res.json({
+					error: true,
+					login: false,
+					message: "Incorrect password",
+				});
+			}
+		} else {
+			return res.json({
+				error: true,
+				login: false,
+				message: "Email not registered!",
+			});
+		}
+	} catch (error) {
+		console.error(error);
+		return res.json({
+			error: true,
+			message: "An error occurred while processing your request",
+		});
+	}
 });
 
 module.exports = router;
